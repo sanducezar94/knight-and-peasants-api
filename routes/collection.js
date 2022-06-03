575125d6f2feaca526cd24f3953b2910552a5d2c
Sandu Cezar <hopeslicer@gmail.com>
2022-06-03 09:51:27 +0300

    metadata fdix

--- routes/collection.js
+++ routes/collection.js
@@ -1,32 +1,110 @@
-const path = require('path');
+const fs = require('fs');
+const fsPromises = fs.promises;
+const axios = require('axios');
+
+var knightMetadata = [];
+var peasantMetadata = [];
+var itemList = ['purses'];
+var itemMetadata = {
+  purses: []
+};
+
+async function fetchMetadata() {
+  const knightMetadataBytes = await fsPromises.readFile('./data/metadata/knights.json');
+  knightMetadata = JSON.parse(knightMetadataBytes);
+
+  const peasantMetadataBytes = await fsPromises.readFile('./data/metadata/peasants.json');
+  peasantMetadata = JSON.parse(peasantMetadataBytes);
+
+  itemList.forEach(async (item) => {
+    const itemMetadataBytes = await fsPromises.readFile(`./data/metadata/${item}.json`);
+    itemMetadata[item] = JSON.parse(itemMetadataBytes);
+  });
+}
+
+setInterval(async () => {
+  await fetchMetadata();
+}, 1000 * 3600 * 0.11);
+
+setTimeout(async () => {
+  await fetchMetadata();
+}, 100);
 
 const collectionEndpoints = (app) => {
-
-    app.get('/api/collectionData', async function (req, res, next) {
-        try{
-          fs.readFile('./stats/collectionData.json', async (err, data) => {
-              let jsonData = JSON.parse(data);
-              return res.send({data: jsonData.uniqueHolder});
-          });
-        }
-        catch(err){
-          console.log(err);
-          return res.status(500).send('Internal Error');
-        }
+  app.get('/api/collectionData', async function (req, res, next) {
+    try {
+      fs.readFile('./stats/collectionData.json', async (err, data) => {
+        let jsonData = JSON.parse(data);
+        return res.send({
+          data: jsonData.uniqueHolder
+        });
       });
-      
-      app.get('/api/getCollectionMetadata', async function (req, res, next) {
-        try{
-          let collectionName = req.query["collectionName"];  
-          const fileBytes = await fsPromises.readFile(`./data/metadata/${collectionName}.json`);
-          return res.send({data: JSON.parse(fileBytes)});
-        }
-        catch(err){
-          console.log(err);
-          return res.status(500).send('Internal Error');
-        }
+    } catch (err) {
+      console.log(err);
+      return res.status(500).send('Internal Error');
+    }
+  });
+
+  app.get('/api/getCollectionMetadata', async function (req, res, next) {
+    try {
+      let collectionName = req.query["collectionName"];
+      const fileBytes = await fsPromises.readFile(`./data/metadata/${collectionName}.json`);
+      return res.send({
+        data: JSON.parse(fileBytes)
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).send('Internal Error');
+    }
+  });
+
+  app.get('/api/accountInfo', async function (req, res, next) {
+    try {
+      const account = req.query.account.toLowerCase();
+
+      const data = await axios.post('https://graph.knightsandpeasants.one/tokens', {
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        referrerPolicy: 'no-referrer',
+        query: `
+            {
+              accountBalances(where: {account: "${account}"}) {
+                amount
+                token {
+                  id
+                  name
+                }
+              }
+              accountInventories(where: {account: "${account}"}) {
+                asset {
+                  id
+                  name
+                }
+              }
+              nftItems(where: {owner: "${account}"}) {
+                  id
+                  tokenId
+                  tokenUri
+                }
+              }
+          `
       });
 
+      const returnData = {
+        accountBalances: data.data.data.accountBalances,
+        accountInventories: data.data.data.accountInventories,
+        nftItems: data.data.data.nftItems
+      }
+
+      return res.status(200).send(returnData);
+
+    } catch (err) {
+      console.log(err);
+      return res.status(500).send('Internal Error');
+    }
+  });
 }
 
 module.exports = collectionEndpoints;
--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const fs = require('fs');
-const fsPromises = fs.promises;
 const bodyParser = require("body-parser");
-const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -15,85 +12,8 @@ app.use(cors());
 require('./routes/knights')(app);
 require('./routes/peasants')(app);
 require('./routes/bags')(app);
-
-// functions used in endpoints
-var knightMetadata = [];
-var peasantMetadata = [];
-var itemList = ['purses'];
-var itemMetadata = {
-  purses: []
-};
-
-async function fetchMetadata() {
-  const knightMetadataBytes = await fsPromises.readFile('./data/metadata/knights.json');
-  knightMetadata = JSON.parse(knightMetadataBytes);
-
-  const peasantMetadataBytes = await fsPromises.readFile('./data/metadata/peasants.json');
-  peasantMetadata = JSON.parse(peasantMetadataBytes);
-
-  itemList.forEach(async (item) => {
-    const itemMetadataBytes = await fsPromises.readFile(`./data/metadata/${item}.json`);
-    itemMetadata[item] = JSON.parse(itemMetadataBytes);
-  });
-}
-
-setInterval(async () => {
-  await fetchMetadata();
-}, 1000 * 3600 * 0.11);
-
-setTimeout(async () => {
-  await fetchMetadata();
-}, 100);
-
-app.get('/api/accountInfo', async function (req, res, next) {
-  try {
-    const account = req.query.account.toLowerCase();
-
-    const data = await axios.post('https://graph.knightsandpeasants.one/tokens', {
-      mode: 'cors',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      referrerPolicy: 'no-referrer',
-      query: `
-      {
-        accountBalances(where: {account: "${account}"}) {
-          amount
-          token {
-            id
-            name
-          }
-        }
-        accountInventories(where: {account: "${account}"}) {
-          asset {
-            id
-            name
-          }
-        }
-        nftItems(where: {owner: "${account}"}) {
-            id
-            tokenId
-            tokenUri
-          }
-        }
-    `
-    });
-    
-    const returnData = {
-      accountBalances: data.data.data.accountBalances,
-      accountInventories: data.data.data.accountInventories,
-      nftItems: data.data.data.nftItems
-    }
-
-    return res.status(200).send(returnData);
-
-  } catch (err) {
-    console.log(err);
-    return res.status(500).send('Internal Error');
-  }
-});
+require('./routes/collection')(app);
 
 app.listen(8000, async function () {
-  await fetchMetadata();
   console.log('API running on port 8000');
 });
